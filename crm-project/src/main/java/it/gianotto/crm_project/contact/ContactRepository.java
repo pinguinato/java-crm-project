24e126cf8372c84e8b116058dc0c0bb7c151393f
Roberto Gianotto <gianottoroberto@gmail.com>
2025-08-29 14:35:49 +0200

    add search by email

--- crm-project/src/main/java/it/gianotto/crm_project/contact/ContactRepository.java
+++ crm-project/src/main/java/it/gianotto/crm_project/contact/ContactRepository.java
@@ -3,7 +3,10 @@ package it.gianotto.crm_project.contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
+    Optional<Contact> findByEmail(String email);
 }
