98fd552b26bf6676ec02acf18d55989a46229ca6
Roberto Gianotto <gianottoroberto@gmail.com>
2025-09-27 15:00:46 +0200

    Inizio sviluppo del controller dei Contatti

---
+++ crm-project/src/main/java/it/gianotto/crm_project/contact/controller/ContactController.java
@@ -0,0 +1,30 @@
+package it.gianotto.crm_project.contact.controller;
+
+import it.gianotto.crm_project.contact.service.ContactService;
+import it.gianotto.crm_project.contact.service.dto.ContactDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("api/v1/contacts")
+public class ContactController {
+
+
+    private final ContactService contactService;
+
+    @GetMapping
+    public ResponseEntity<List<ContactDTO>> getAllContacts() {
+
+        List<ContactDTO> contacts = contactService.getContacts();
+
+        return ResponseEntity.ok(contacts);
+    }
+}
