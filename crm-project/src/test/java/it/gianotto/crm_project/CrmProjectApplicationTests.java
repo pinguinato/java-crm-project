12bf6ca2b3863c9ff83be55e067dc23733fd45d2
Roberto Gianotto <gianottoroberto@gmail.com>
2025-09-28 16:10:09 +0200

    fix Docker setup with local Postgres installation, fix unit-test and docker env, fix Dockerfile, fix local startup properties

--- crm-project/src/test/java/it/gianotto/crm_project/CrmProjectApplicationTests.java
+++ crm-project/src/test/java/it/gianotto/crm_project/CrmProjectApplicationTests.java
@@ -2,8 +2,10 @@ package it.gianotto.crm_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("unit_test")
 class CrmProjectApplicationTests {
 
 	@Test
