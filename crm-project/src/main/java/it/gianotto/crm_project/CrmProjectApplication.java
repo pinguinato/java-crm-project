64507d3590e4709567387c543d781deb43ed1654
Roberto Gianotto <gianottoroberto@gmail.com>
2025-09-28 09:12:12 +0200

    rimozione logo di spring, aggiunta messaggio allo startup del micro servizio e controller per il crud del modulo contatti

--- crm-project/src/main/java/it/gianotto/crm_project/CrmProjectApplication.java
+++ crm-project/src/main/java/it/gianotto/crm_project/CrmProjectApplication.java
@@ -1,7 +1,9 @@
 package it.gianotto.crm_project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CrmProjectApplication {
@@ -9,5 +11,14 @@ public class CrmProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrmProjectApplication.class, args);
 	}
-
+	
+	@PostConstruct
+	public void init() {
+		String springVersion = SpringBootVersion.getVersion();
+		System.out.println("==================================================");
+		System.out.println(">> Spring Boot Version: " + springVersion);
+		System.out.println(">> START CRM PROJECT NOW");
+        System.out.println(">> Copyright 2025 - gianottoroberto@gmail.com");
+		System.out.println("==================================================");
+	}
 }
